67aacabe1e4c12a9ba7e588c74563b8723ea2b15
Vawser <ben.rouncefield@gmail.com>
2025-04-16 05:06:27 +0100

    Mod Engine
    - Added the ability to automatically launch a mod via ModEngine2, assuming the path to its executable has been set by the user

--- Smithbox.Core/Utils/ModEngineUtils.cs
+++ Smithbox.Core/Utils/ModEngineUtils.cs
@@ -12,44 +12,56 @@ namespace Smithbox.Core.Utils;
 
 public static class ModEngineUtils
 {
+    /// <summary>
+    /// A bit hacky, but good enough for now
+    /// </summary>
+    /// <param name="curProject"></param>
     public static void LaunchMod(Project curProject)
     {
         var modEngineInstallFolderPath = Path.GetDirectoryName(CFG.Current.ModEngineInstall);
         var modTomlPath = @$"{modEngineInstallFolderPath}\smithbox_launch_config.toml";
 
-        if (!File.Exists(modTomlPath))
-        {
-            string tomlString = $@"[modengine]
+        var looseParams = "false";
+        if (CFG.Current.UseLooseParams)
+            looseParams = "true";
+
+        string tomlString = $@"[modengine]
 debug = false
 external_dlls = []
 
 [extension.mod_loader]
 enabled = true
-loose_params = {CFG.Current.UseLooseParams}
+loose_params = {looseParams}
 
 mods = [
-    {{ enabled = true, name = ""{curProject.ProjectName}"", path = ""{curProject.ProjectPath}"" }}
+    {{ enabled = true, name = ""{curProject.ProjectName}"", path = ""{curProject.ProjectPath.Replace("\\", "\\\\")}"" }}
 ]
 
 [extension.scylla_hide]
 enabled = false";
 
-            File.WriteAllText(modTomlPath, tomlString);
-        }
+        File.WriteAllText(modTomlPath, tomlString);
 
         if(File.Exists(modTomlPath))
         {
+            var tomlPath = $@"{modEngineInstallFolderPath}\smithbox_launch_config.toml";
             var projectType = $"{curProject.ProjectType}".ToLower();
+
+            var inputStr = $"'-t' '{projectType}' '-c' '{tomlPath}'".Replace("'", "\"");
+
+            TaskLogs.AddLog(inputStr);
 
             ProcessStartInfo psi = new ProcessStartInfo
             {
-                FileName = "cmd.exe",
-                Arguments = @$"{CFG.Current.ModEngineInstall} -t {projectType} -c {modTomlPath}",
+                FileName = CFG.Current.ModEngineInstall,
+                Arguments = inputStr,
+                UseShellExecute = false,
                 RedirectStandardOutput = true,
                 RedirectStandardError = true,
-                UseShellExecute = false,
                 CreateNoWindow = true
             };
+
+            Process.Start(psi);
         }
     }
 }
